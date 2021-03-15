e202b5724d5e0a8ea655c9f015654cd8e9205942
Fukubara <60829788+Fukubara@users.noreply.github.com>
2021-03-15 16:09:32 -0300

    Utilizando cookies para guardar as marcacoes

--- src/components/ConjuntoTarefas.tsx
+++ src/components/ConjuntoTarefas.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import styles from '../styles/BlocoTarefa.module.css'
+
+import Tarefa from './Tarefa'
 
 interface Tarefa {
     id: number,
@@ -11,40 +12,22 @@ interface Tarefa {
 
 interface PropsTarefa{
     tarefa: Tarefa[]
+    check: any
 }
 
 const ConjuntoTarefas: React.FC<PropsTarefa> = (props) => {
-
     const [Tps, setTps] = useState<Tarefa[]>()
-
+    
     useEffect(() => {
         setTps(props.tarefa)
     }, [])
-
+    
     return (
         <>
             {
                 Tps?.map((tp) => {
                     return (
-                        <div className={styles.tarefaContainer}>
-                            <div className={styles.left}>
-                                <div><strong>Matéria: </strong>{tp.materia}</div>
-                                <div><strong>Atividade: </strong>{tp.descricao}</div>
-                                <div>
-                                    <span><strong>Local de envio: </strong>{tp.local}</span>
-                                    <span className={styles.data}><div><strong>Data de Entrega: </strong>{tp.data}</div></span>
-                                </div>
-                            </div>
-                            <div>
-                                <input type="checkbox" id={`cbx ${tp.id}`} className={styles.cbx} />
-                                <label htmlFor={`cbx ${tp.id}`} className={styles.check}>
-                                <svg width="18px" height="18px" viewBox="0 0 18 18">
-                                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
-                                    <polyline points="1 9 7 14 15 4"></polyline>
-                                </svg>
-                                </label>
-                            </div>
-                        </div>
+                        <Tarefa key={tp.id} check={props.check[tp.id]} objtp={tp} />
                     )
                 })
             }
---
+++ src/components/Tarefa.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import cookie from 'next-cookies'
+
+import styles from '../styles/BlocoTarefa.module.css'
+
+const Tarefa = (props: any) => {
+
+    const [Checked, setChecked] = useState(JSON.parse(props.check))
+
+    useEffect(() => {
+        document.cookie = `${props.objtp.id}=${Checked}; path='/' `
+    }, [Checked])
+
+    return (
+        <div className={styles.tarefaContainer}>
+            <div className={styles.left}>
+                <div><strong>Matéria: </strong>{props.objtp.materia}</div>
+                <div><strong>Atividade: </strong>{props.objtp.descricao}</div>
+                <div>
+                    <span><strong>Local de envio: </strong>{props.objtp.local}</span>
+                    <span className={styles.data}><div><strong>Data de Entrega: </strong>{props.objtp.data}</div></span>
+                </div>
+            </div>
+            <div>
+                <input 
+                    type="checkbox" 
+                    id={`cbx ${props.objtp.id}`} 
+                    className={styles.cbx}
+                    checked={Checked}
+                    onChange={e => setChecked(e.target.checked)}
+                />
+                <label htmlFor={`cbx ${props.objtp.id}`} className={styles.check}>
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                </label>
+            </div>
+        </div>
+    );
+}
+
+export default Tarefa;
